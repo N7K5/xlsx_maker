eb9bc631ed6c05e57c04fe77310833a45e78d40e
Daipayan Chakder <johnchakder@gmail.com>
2018-07-20 15:39:52 +0530

    partially Done...

---
+++ convert_to_json.js
@@ -0,0 +1,63 @@
+const excelToJson = require('convert-excel-to-json');
+
+
+var convert= function(file, req_funs, starting_row) {
+    const ini_tot_result = excelToJson({
+        sourceFile: file,
+    });
+
+    var result= {
+        data: [],
+    }
+
+    var blocks= [ "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                     "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT"];
+
+    var ori_res= ini_tot_result[Object.keys(ini_tot_result)[0]];
+    
+    var property_len= Object.keys(req_funs).length;
+
+    var obj= {A: "S. No"};
+    for(var i=0; i<property_len; i++) {
+        obj[blocks[i]]= Object.keys(req_funs)[i];
+    }
+    result.data.push(obj);
+    
+    
+    for(var cur= 0; cur<ori_res.length; cur++) {
+        var index= cur+1;
+        if(starting_row) {
+            if(cur<starting_row) {
+                continue;
+            }
+            index-=starting_row;
+        }
+        var stud= ori_res[cur];
+        // console.log("\n\n"+JSON.stringify(stud, undefined, 2)+ "\n\n");
+        obj= {"A": index};
+
+        for(var i=0; i<property_len; i++) {
+            var fn= req_funs[Object.keys(req_funs)[i]];
+            if(typeof(fn)!= "function") {
+                obj[blocks[i]]= fn;
+            }
+            else {
+                let tmp_res= fn(stud);
+                if(tmp_res== null || tmp_res== undefined) {
+                    obj[blocks[i]]= "N/A";
+                }
+                else {
+                    obj[blocks[i]]= tmp_res;
+                }
+            }
+        }
+        result.data.push(obj);
+    }
+
+    return result;
+
+}
+
+module.exports= {
+    convert,
+}
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 
 const excelToJson = require('convert-excel-to-json');
 
+const convert_to_json= require("./convert_to_json");
+
 sourceFile= __dirname + "/IT_old.xls";
 
-const result = excelToJson({
-    sourceFile: sourceFile,
-});
+
+var req_funs= {
+    "Name":(obj) => obj.C,
+    "Institution": (obj) => "Govt. College Of Engg And Leather Tech",
+    "Roll no": (obj) => obj.AI,
+    "Gender": (obj) => obj.D,
+    "DOB": (obj) => obj.E,
+    "10th Marks": (obj) => obj.H,
+    "10th Year of Passing": (obj) => obj.I,
+    "12th Marks": (obj) => obj.K,
+    "12th Year of Passing": (obj) => obj.L,
+    "Grad. avg Marks": (obj) => ((obj.W+ obj.X+ obj.Y+ obj.Z+ obj.AA)/5),
+    "Grad year of passing": (obj) => 2019,
+    "Stream": (obj) => "IT",
+    "Degree": (obj) => "B. Tech",
+    "BackLogs": (obj) => "N/A",
+    "Placement Status": (obj) =>"N/A",
+    "Certification": (obj) => "N/A",
+    "E-Mail": (obj) => obj.F,
+    "Contact": (obj) => obj.G,
+}
+
+
+
+const result = convert_to_json.convert(sourceFile, req_funs, 2);
 
 console.log(JSON.stringify(result, undefined, 2));
